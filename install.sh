7f815504e88f1c52abcaddacbb86cffe26d0e993
Yuya.Nishida. <yuya@j96.org>
2026-03-21 23:37:07 +0900

    chore: add install script

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+set -eu
+
+REF="${DOT_DEVCONTAINER_REF:-master}"
+TARBALL_URL="https://github.com/nishidayuya/dot-devcontainer/archive/${REF}.tar.gz"
+
+echo "Installing .devcontainer files from ${REF}..."
+
+# Remove existing .devcontainer directory without checking
+rm -rf .devcontainer
+
+echo "Downloading and extracting..."
+# GitHub's tarball structure: dot-devcontainer-<ref>/.devcontainer/
+# Use --strip-components=1 to remove the top-level directory prefix.
+curl -f -sL "${TARBALL_URL}" | tar -xz --strip-components=1 "dot-devcontainer-${REF}/.devcontainer"
+
+echo "Done!"
